3c3740127bc5c6e8cb6d851c47c535e17c3fb66e
shivarajloni <shivarajbloni@gmail.com>
2023-08-21 16:22:41 +0530

    simple portfolio complete

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos repellendus, laboriosam autem sint aut doloremque exercitationem porro quaerat eum aliquam animi, veniam iste officia. Iure voluptatum minus voluptates enim ipsum, impedit optio culpa. Rem dolor doloribus quod facilis repudiandae ipsum dignissimos, dicta hic expedita explicabo dolorum, laudantium itaque impedit quisquam?
+        </p>
+
+        <br />
+        <p className='text-xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur optio odio saepe, accusantium eum reprehenderit est, nesciunt, hic reiciendis blanditiis veniam a ducimus nobis doloremque exercitationem atque quo aliquid. Tempora corporis eius in. Iure aspernatur amet obcaecati sit nostrum officiis. Cumque consequatur incidunt illum adipisci odit aut dolorem consequuntur necessitatibus?
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
